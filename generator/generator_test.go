bdd9ecf90497178774c82edad444a068b8f6331d
Ben Johnson <benbjohnson@yahoo.com>
2013-10-06 15:34:26 -0600

    go fmt

--- generator/generator_test.go
+++ generator/generator_test.go
@@ -1,24 +1,24 @@
 package generator
 
 import (
-    "bytes"
-    "go/ast"
-    "go/parser"
-    "go/token"
-    "github.com/stretchr/testify/assert"
-    "testing"
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"testing"
 )
 
 func TestGeneratorGenerate(t *testing.T) {
-    var b bytes.Buffer
-    src := `
-    package foo
-    type Foo struct {
-        Name string
-    }
-    `
-    f, _ := parser.ParseFile(token.NewFileSet(), "foo.go", src, 0)
-    err := GenerateEncoder("foo", f.Decls[0].(*ast.GenDecl).Specs[0].(*ast.TypeSpec), &b)
-    assert.Nil(t, err)
-    assert.Equal(t, b.String(), "XXX")
+	var b bytes.Buffer
+	src := `
+package foo
+type Foo struct {
+    Name string
+}
+`
+	f, _ := parser.ParseFile(token.NewFileSet(), "foo.go", src, 0)
+	err := GenerateEncoder("foo", f.Decls[0].(*ast.GenDecl).Specs[0].(*ast.TypeSpec), &b)
+	assert.Nil(t, err)
+	assert.Equal(t, b.String(), "XXX")
 }
